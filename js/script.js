c145c423dead3b31b6ec8705ac79b4de19c5d07b
jr-iannuzzi <josericardoiannuzzi@hotmail.com>
2025-05-21 17:35:33 -0300

    Agendamento: HTML, CSS e JS

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Botão da página inicial
+document.getElementById("agendarBtn").addEventListener("click", function() {
+    alert("Redirecionando para a página de agendamento de consulta...");
+    window.location.href = "agendamento.html";
+});
+
+// Formulário de agendamento
+document.getElementById("agendamentoForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const data = document.getElementById("data").value;
+  const hora = document.getElementById("hora").value;
+  const medico = document.getElementById("medico").value;
+
+  if (!nome || !data || !hora || !medico) {
+    alert("Por favor, preencha todos os campos antes de agendar sua consulta.");
+    return;
+  }
+
+  alert(`Consulta agendada com sucesso!\nNome: ${nome}\nData: ${data}\nHorário: ${hora}\nMédico: ${medico}`);
+  
+  this.reset();
+});
